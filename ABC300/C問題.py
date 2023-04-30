75ce2d64a937307a6b1c753ca5451f2310a0d1cd
Kabocha13 <107018575+Kabocha13@users.noreply.github.com>
2023-04-30 14:20:56 +0900

    Update C問題.py

--- ABC300/C問題.py
+++ ABC300/C問題.py
@@ -2,4 +2,36 @@
 # その値の最大値が答えになることがわかる
 # 探索済みの#を他の文字列に変えなければ重複して計算してしまう
 
-未実装
+import sys
+
+H, W = map(int, input().split())
+g = []
+for i in range(H):
+    g.append(input().strip())
+
+def ok(i, j):
+    return 0 <= i < H and 0 <= j < W
+
+def test(i, j, d):
+    for x in [+d, -d]:
+        for y in [+d, -d]:
+            s, t = i + x, j + y
+            if not ok(s, t) or g[s][t] != '#':
+                return False
+    return True
+
+N = min(H, W)
+ans = [0] * (N + 1)
+
+for i in range(H):
+    for j in range(W):
+        if g[i][j] != '#':
+            continue
+        if test(i, j, 1):
+            d = 1
+            while test(i, j, d + 1):
+                d += 1
+            ans[d] += 1
+
+for i in range(1, N + 1):
+    print(ans[i], end=' ' if i < N else '\n')
