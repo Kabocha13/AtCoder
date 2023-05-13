5431f2bd51cb50843cdb42aa1aa2fc0608cf6414
Kabocha13 <107018575+Kabocha13@users.noreply.github.com>
2023-05-14 00:37:20 +0900

    Create A問題.py

---
+++ ABC301/A問題.py
@@ -0,0 +1,11 @@
+n = int(input())
+s = input()
+if s.count("T")<s.count("A"):
+  print("A")
+elif s.count("A")==s.count("T"):
+  if s[-1]=="A":
+    print("T")
+  else:
+    print("A")
+else:
+  print("T")
